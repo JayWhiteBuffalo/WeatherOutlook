b9eee770dada864a9c3a98820778ba97de492ddc
White Buffalo <apothocare@gmail.com>
2022-08-21 02:01:34 -0500

    HTML structure layout. Psudeo Coded

---
+++ assets/js/script.js
@@ -0,0 +1,31 @@
+let apiKey = 
+
+/* search logic */
+
+function search(city){
+
+    
+}
+
+//functions
+    //link text box to api settings and auto suggest if possible
+    //Take user input city and retrive API
+    //Display API
+    //Store user city search in local storage
+    //Create a button in the recent searches section
+    //load recent searches from local storage on pageload
+    //When Recent storage buttons clicked update weather data
+    //Populate 7 day forcast
+
+
+
+
+
+
+
+
+
+
+
+//Button Event
+$('searchBtn').on('click', console.log("This works"));
