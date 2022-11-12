5457fd36272e1989b087cd0200a65bf8eb7ec28f
White Buffalo <apothocare@gmail.com>
2022-11-12 13:17:59 -0600

    City input is logging return data from api correctly

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,31 +1,31 @@
-let apiKey = 
 
-/* search logic */
+const searchBtn = document.getElementById('searchBtn');
+//const city = cityInput.val().trim();
+//const apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=f94d7942393e88c574f5bb107287fd0f`;
+let searchHistoryArr = [];
 
-function search(city){
+let now = moment().format('L');
 
-    
+const handleSubmit = (e) => {
+  if (e)
+  e.preventDefault();
+  let cityInput = document.getElementById('city-search').value
+  currentWeather(cityInput);
 }
+//Get current weather
+function currentWeather(cityInput) {
+  const apiCurrentUrl = `https://api.openweathermap.org/data/2.5/weather?q=${cityInput}&units=imperial&appid=f94d7942393e88c574f5bb107287fd0f`;
+  $.ajax({
+      url: apiCurrentUrl,
+      method: "GET"
+  }).then(function (response) {
+    console.log(response);
 
-//functions
-    //link text box to api settings and auto suggest if possible
-    //Take user input city and retrive API
-    //Display API
-    //Store user city search in local storage
-    //Create a button in the recent searches section
-    //load recent searches from local storage on pageload
-    //When Recent storage buttons clicked update weather data
-    //Populate 7 day forcast
-
-
-
-
-
-
-
+  })
+}
 
 
 
 
 //Button Event
-$('searchBtn').on('click', console.log("This works"));
+searchBtn.addEventListener("click", handleSubmit);
