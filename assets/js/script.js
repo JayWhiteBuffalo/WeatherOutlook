316fc9268ae902474a27f5461d5eeb39e8044938
White Buffalo <apothocare@gmail.com>
2022-11-13 13:48:59 -0600

    Cards append in correct div, style is okay. for loop break params need to be fixed. Image needs to be added to forecast cards.

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,17 +13,21 @@ const handleSubmit = (e) => {
   currentWeather(cityInput);
 }
 
-//popurlate page function
+//
+
+//populate page function
 function populateSearch(response) {
-  console.log(response);
+  //console.log(response);
   let cityTitle = response.name;
   let cityTemp = response.main.temp;
   let cityWind = response.wind.speed;
   let cityHumidity = response.main.humidity;
+  let weatherImg = response.weather.main;
   document.getElementById('city-title').innerHTML = cityTitle;
-  document.getElementById('city-temp').innerHTML = cityTemp;
-  document.getElementById('city-wind').innerHTML = cityWind;
-  document.getElementById('city-humidity').innerHTML = cityHumidity;
+  document.getElementById('city-temp').innerHTML = "Temperature : " + cityTemp + " *F";
+  document.getElementById('city-wind').innerHTML = "Wind : " + cityWind + " mph";
+  document.getElementById('city-humidity').innerHTML = "Humidity : " + cityHumidity + " %";
+  weatherForcast(response);
 };
 //Get current weather
 function currentWeather(cityInput) {
@@ -33,7 +37,47 @@ function currentWeather(cityInput) {
       method: "GET"
   }).then(function (response) {
     populateSearch(response);
-})};
+  })
+};
+
+//Get 5 Day Forcast
+function weatherForcast(response) {
+  let lat = response.coord.lat;
+  let lon = response.coord.lon;
+
+  const forcastCall = `https://api.openweathermap.org/data/3.0/onecall?lat=`+lat+`&lon=`+lon+`&units=imperial&appid=f94d7942393e88c574f5bb107287fd0f`;
+  fetch(forcastCall)
+      .then((response) => {
+        return response.json()
+      })
+      .then((data) =>{
+          pop5daycards(data)});
+  
+};
+
+function pop5daycards(data) {
+  for (let i =1; i < data.daily.length; i++) {
+    if(i === 6) {break;}
+    let day = moment().add(i, 'd').format('MMMM do YYYY');
+    let card = document.createElement('div');
+        card.classList.add('forecast-card');
+    let cardDate = document.createElement('b');
+        cardDate.classList.add('forecast-date');
+        cardDate.innerText = day;
+    let cardInfo = document.createElement('div');
+        cardInfo.classList.add('forecast-info');
+    let cardTemp = document.createElement('p');
+        cardTemp.innerText = ("High Temp :" + data.daily[i].temp.max);
+    let cardWind = document.createElement('p');
+        cardWind.innerText = ("Wind : " + data.daily[i].wind_speed + "MPH");
+    let cardHumid = document.createElement('p');
+        cardHumid.nnerText = ("Humidity : " + data.daily[i].humidity);
+
+    card.append(cardDate,cardInfo,cardTemp,cardWind,cardHumid);
+    let container = document.getElementById('card-container');
+    container.appendChild(card);
+  }
+};
 
 
 
